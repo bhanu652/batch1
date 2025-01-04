e9e1833bfdc89c984d965a62fa73b0468ae80d0e
bhanu652 <bhanurowdy57@gmail.com>
2025-01-04 12:32:14 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+class Demo2{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        if(a>b){
+            System.out.println(a);
+        }
+        else{
+            System.out.print(b);
+        }
+    }
+}
